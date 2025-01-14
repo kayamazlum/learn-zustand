0a3aa205a23a2a5677b3f37ad57bd323b9562253
kayamazlum <mazlum09kaya@gmail.com>
2025-01-14 22:51:30 +0300

    counter pratik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useUserStore } from "@/store/user-store";
 
 export default function Home() {
-  const { user, update } = useUserStore();
+  const { user, update, counter, increment, decrement } = useUserStore();
 
   const handleUpdateUser = () => {
     const user = {
@@ -20,6 +20,16 @@ export default function Home() {
       <button onClick={handleUpdateUser} className="p-4 bg-blue-600 rounded-sm">
         Update User
       </button>
+
+      <p className="mt-6">Counter: {counter}</p>
+      <div className="flex gap-2">
+        <button className="p-2 bg-yellow-500" onClick={increment}>
+          Increment
+        </button>
+        <button className="p-2 bg-yellow-500" onClick={decrement}>
+          Decrement
+        </button>
+      </div>
     </main>
   );
 }
--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -9,9 +9,21 @@ interface User {
 interface UserStore {
   user: User | null;
   update: (user: User) => void;
+  counter: number;
+  increment: () => void;
+  decrement: () => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   user: null,
+  counter: 0,
   update: (user) => set(() => ({ user })),
+  increment: () =>
+    set((state) => ({
+      counter: state.counter + 1,
+    })),
+  decrement: () =>
+    set((state) => ({
+      counter: state.counter - 1,
+    })),
 }));
